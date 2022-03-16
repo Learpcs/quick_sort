3dcbcc773583ec3f8322210d085b1bcc8ad0fe4d
Learpcs <learpcs@gmail.com>
2022-03-16 17:24:43 +0300

    Done.

--- 1.cpp
+++ 1.cpp
@@ -1,35 +1,37 @@
-#include <vector>
 #include <algorithm>
+#include <climits>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-vector<int> a{5, 1, 3, 2, 4};
+vector<int> a{5, 1, 1, 1, 4};
 
-void quick_sort(int l, int r)
-{
-	if(r - l <= 0)
-		return;
-	int pivot = a[l];
-	int pl = l, pr = r;
-	while(pl <= pr)
-	{
-		while(a[pl] < pivot)
-			++pl;
-		while(a[pr] >= pivot)
-			--pr;
-		if(pl < pr)
-			swap(a[pl], a[pr]);
-
-	}
-	quick_sort(l, pl - 1);
-	quick_sort(pl, r);
+void quick_sort(int l, int r) {
+  int pivot = INT_MAX;
+  for (int i = l + 1; i <= r; ++i) {
+    if (a[i] != a[l]) {
+      pivot = max(a[i], a[l]);
+      break;
+    }
+  }
+  if (pivot == INT_MAX) {
+    return;
+  }
+  int pl = l, pr = r;
+  while (pl <= pr) {
+    while (a[pl] < pivot)
+      ++pl;
+    while (a[pr] >= pivot)
+      --pr;
+    if (pl < pr)
+      swap(a[pl], a[pr]);
+  }
+  quick_sort(l, pl - 1);
+  quick_sort(pl, r);
 }
 
-int main()
-{
-	cout << "lol" << flush;
-	quick_sort(0, a.size() - 1);
-	cout << "lol";
-	for(int& x : a)
-		cout << x << ' ';
+int main() {
+  quick_sort(0, a.size() - 1);
+  for (int &x : a)
+    cout << x << ' ';
 }
