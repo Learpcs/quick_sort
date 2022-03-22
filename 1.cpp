f3519c3d436ede00008e7e0cb3a938c19e59f99d
Learpcs <learpcs@gmail.com>
2022-03-22 12:33:56 +0300

    Effective (actually not) partitioning done

--- 1.cpp
+++ 1.cpp
@@ -1,37 +1,29 @@
-#include <algorithm>
-#include <climits>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> a{5, 1, 1, 1, 4};
-
-void quick_sort(int l, int r) {
-  int pivot = INT_MAX;
-  for (int i = l + 1; i <= r; ++i) {
-    if (a[i] != a[l]) {
-      pivot = max(a[i], a[l]);
-      break;
-    }
-  }
-  if (pivot == INT_MAX) {
-    return;
-  }
+void quick_sort(int l, int r, vector<int> &a) {
+  int pivot = a[l];
   int pl = l, pr = r;
   while (pl <= pr) {
-    while (a[pl] < pivot)
+    while (a[pl] < pivot && pl <= r)
       ++pl;
-    while (a[pr] >= pivot)
+    while (a[pr] >= pivot && pr >= l)
       --pr;
     if (pl < pr)
       swap(a[pl], a[pr]);
   }
-  quick_sort(l, pl - 1);
-  quick_sort(pl, r);
+  if (l < pr)
+    quick_sort(l, pr, a);
+  for (int i = pl; i < r; ++i)
+    if (a[i] == pivot)
+      swap(a[i], a[pl++]);
+  if (pl < r)
+    quick_sort(pl, r, a);
 }
 
 int main() {
-  quick_sort(0, a.size() - 1);
+  vector<int> a{0, 1, 0, 1, 0, 2, 1, 0, 0};
+  quick_sort(0, a.size() - 1, a);
   for (int &x : a)
     cout << x << ' ';
 }
